a20625ace68f034c6f1f2fb489249733f94e8893
Whitney Tran <wtran95@gmail.com>
2020-10-03 10:52:00 -0700

    Added Collectables

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectable
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        collectableName = "Chest";
+        description = "increase score by 100";
+    }
+
+    public override void Use()
+    {
+       player.GetComponent<playerManager>().ChangeScore(100);
+       Destroy(this.gameObject);
+    }
+}
